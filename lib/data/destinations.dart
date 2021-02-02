521009e8b8de16c7ffd6167651c345d284fc685a
linus muema <linus.m.muema@gmail.com>
2021-02-02 12:34:20 +0300

    - add spanish destinations

--- lib/data/destinations.dart
+++ lib/data/destinations.dart
@@ -1,17 +1,49 @@
 class Destination {
   String name;
   String image;
+  String country;
 
   Destination({
     this.name,
     this.image,
+    this.country,
   });
 }
 
 // Scotland
-final glasgow =
-    Destination(name: "Glasgow", image: 'assets/images/glasgow.png');
-final edinburgh =
-    Destination(name: "Edinburgh", image: 'assets/images/edinburgh.png');
-final lochNess =
-    Destination(name: "Loch Ness", image: 'assets/images/loch_ness.png');
+final glasgow = Destination(
+  name: "Glasgow",
+  image: 'assets/images/glasgow.png',
+  country: 'Scotland',
+);
+
+final edinburgh = Destination(
+  name: "Edinburgh",
+  image: 'assets/images/edinburgh.png',
+  country: 'Scotland',
+);
+
+final lochNess = Destination(
+  name: "Loch Ness",
+  image: 'assets/images/loch_ness.png',
+  country: 'Scotland',
+);
+
+// Spain
+final laSagradaFamilia = Destination(
+  name: "La Sagrada Familia",
+  image: 'assets/images/la_sagrada_familia.png',
+  country: 'Spain',
+);
+
+final alhambra = Destination(
+  name: "Alhambra",
+  image: 'assets/images/alhambra.png',
+  country: 'Spain',
+);
+
+final museoDelPrado = Destination(
+  name: "Museo del Prado",
+  image: 'assets/images/museo_del_prado.png',
+  country: 'Spain',
+);
