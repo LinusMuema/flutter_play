232efc0cd0b3a0810c992a34a099f2d3ccc516d7
linus muema <linus.m.muema@gmail.com>
2021-02-02 12:18:43 +0300

    - add scotland destinations

---
+++ lib/data/destinations.dart
@@ -0,0 +1,17 @@
+class Destination {
+  String name;
+  String image;
+
+  Destination({
+    this.name,
+    this.image,
+  });
+}
+
+// Scotland
+final glasgow =
+    Destination(name: "Glasgow", image: 'assets/images/glasgow.png');
+final edinburgh =
+    Destination(name: "Edinburgh", image: 'assets/images/edinburgh.png');
+final lochNess =
+    Destination(name: "Loch Ness", image: 'assets/images/loch_ness.png');
