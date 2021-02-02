54a3c43b90d790f4111e00f2ba19adb11e9501a6
linus muema <linus.m.muema@gmail.com>
2021-02-02 16:36:34 +0300

    - added swipe detection

--- lib/screens/country.dart
+++ lib/screens/country.dart
@@ -8,6 +8,7 @@ import 'package:flutter_play/data/countries.dart';
 import 'package:flutter_play/data/destinations.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:measurer/measurer.dart';
+import 'package:swipedetector/swipedetector.dart';
 
 class CountryInfo extends StatefulWidget {
   @override
@@ -36,61 +37,69 @@ class _CountryInfoState extends State<CountryInfo> {
           image: _country.image,
           content: BlurBackground(
               detailsState: _detailsState,
-              content: ContainerGradient(
-                content: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    GestureDetector(
-                      onTap: () => Navigator.pop(context),
-                      child: Container(
-                        margin: EdgeInsets.symmetric(
-                            horizontal: 10, vertical: topPadding),
-                        child: SvgPicture.asset('assets/images/back.svg'),
+              content: SwipeDetector(
+                onSwipeUp: () {
+                  updateDetails(true);
+                },
+                onSwipeDown: () {
+                  updateDetails(false);
+                },
+                child: ContainerGradient(
+                  content: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: topPadding),
+                          child: SvgPicture.asset('assets/images/back.svg'),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: Stack(children: [
-                        Positioned(
-                          bottom: 0.0,
-                          left: 0.0,
-                          child: Measurer(
-                            onMeasure: (size, constraints) {
-                              setChildHeight(size.height);
-                            },
-                            child: AnimatedOpacity(
-                              opacity: _opacity,
-                              duration: Duration(milliseconds: 500),
-                              child: _detailsState ? details : Container(),
+                      Expanded(
+                        child: Stack(children: [
+                          Positioned(
+                            bottom: 0.0,
+                            left: 0.0,
+                            child: Measurer(
+                              onMeasure: (size, constraints) {
+                                setChildHeight(size.height);
+                              },
+                              child: AnimatedOpacity(
+                                opacity: _opacity,
+                                duration: Duration(milliseconds: 500),
+                                child: _detailsState ? details : Container(),
+                              ),
+                            ),
+                          ),
+                          AnimatedPositioned(
+                            bottom: _detailsState ? contentHeight : 0.0,
+                            left: 0.0,
+                            duration: Duration(milliseconds: 300),
+                            child: Heading(
+                              heading: _country.name,
+                              cap: caption,
+                              color: Colors.white,
+                              large: true,
+                            ),
+                          ),
+                        ]),
+                      ),
+                      Rotate(
+                        animate: _detailsState,
+                        content: GestureDetector(
+                          onTap: () => updateDetails(!_detailsState),
+                          child: Container(
+                            height: 24,
+                            margin: EdgeInsets.symmetric(vertical: 20),
+                            child: Center(
+                              child: SvgPicture.asset('assets/images/more.svg'),
                             ),
                           ),
                         ),
-                        AnimatedPositioned(
-                          bottom: _detailsState ? contentHeight : 0.0,
-                          left: 0.0,
-                          duration: Duration(milliseconds: 300),
-                          child: Heading(
-                            heading: _country.name,
-                            cap: caption,
-                            color: Colors.white,
-                            large: true,
-                          ),
-                        ),
-                      ]),
-                    ),
-                    Rotate(
-                      animate: _detailsState,
-                      content: GestureDetector(
-                        onTap: () => showDetails(),
-                        child: Container(
-                          height: 24,
-                          margin: EdgeInsets.symmetric(vertical: 20),
-                          child: Center(
-                            child: SvgPicture.asset('assets/images/more.svg'),
-                          ),
-                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               )),
         ),
@@ -104,10 +113,10 @@ class _CountryInfoState extends State<CountryInfo> {
     });
   }
 
-  showDetails() {
+  updateDetails(bool show) {
     setState(() {
-      _detailsState = !_detailsState;
-      _opacity = _detailsState ? 1.0 : 0.0;
+      _detailsState = show;
+      _opacity = show ? 1.0 : 0.0;
     });
   }
 }
